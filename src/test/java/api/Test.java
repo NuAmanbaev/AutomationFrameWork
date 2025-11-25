cb21bc8242af6e3dc9ed39c87c00ff70b4d2bb8d
NuAmanbaev <amanbaev62@gmail.com>
2025-11-24 19:40:00 -0600

    Completed Frist Part

--- src/test/java/api/Test.java
+++ src/test/java/api/Test.java
@@ -1,21 +1,33 @@
 package api;
 
+import com.github.javafaker.Faker;
 import entities.RequestBody;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Assert;
+import org.openqa.selenium.json.Json;
 import utilities.CashWiseToken;
 import utilities.Config;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Test {
+
 
     @org.junit.Test
     public void testToken(){
+
+        RequestBody requestBody = new RequestBody();
+
+
         String endPoint = "https://backend.cashwise.us/api/myaccount/auth/login";
 
 
-        RequestBody requestBody = new RequestBody();
+
         requestBody.setEmail(Config.getProperty("loginEmail"));
         requestBody.setPassword(Config.getProperty("loginPassword"));
 
@@ -47,8 +59,119 @@ public class Test {
     Response response = RestAssured.given().auth().oauth2(token).get(url);
     response.prettyPrint();
 
+    String expectedEmail = response.jsonPath().getString("email");
+
+    Assert.assertFalse(expectedEmail.isEmpty());
+    Assert.assertTrue(expectedEmail.endsWith(".com"));
+
+
 
 }
+
+@org.junit.Test
+
+    public void  CreateSincleSeller() {
+    Faker faker = new Faker();
+
+        RequestBody requestBody = new RequestBody();
+
+        String endpoint1 = "/api/myaccount/sellers";
+
+        String url = Config.getProperty("cashWiseApiUrl") + "/api/myaccount/sellers";
+
+        String token = CashWiseToken.GetToken();
+
+        requestBody.setCompanyName(faker.name().fullName());
+        requestBody.setSellerName(faker.name().username());
+        requestBody.setEmail(faker.internet().emailAddress());
+        requestBody.setPhoneNumber(faker.number().randomDigit());
+        requestBody.setAddress(faker.address().fullAddress());
+
+
+        Response response1 = RestAssured.given().auth().oauth2(token).contentType(ContentType.JSON)
+                .body(requestBody).post(url);
+
+            int statusCode1 = response1.statusCode();
+
+    System.out.println(statusCode1);
+
+    response1.prettyPrint();
+
+
+
+
+
+}
+
+
+@org.junit.Test
+
+    public void GetAllSellers(){
+
+        String url = Config.getProperty("cashWiseApiUrl") +  "/api/myaccount/sellers";
+        String token = CashWiseToken.GetToken();
+
+
+        Map<String,Object > params = new HashMap<>();
+        params.put("isArchived", false);
+        params.put("size",10);
+        params.put("page", 1);
+
+        Response response = RestAssured.given().auth().oauth2(token).params(params).get(url);
+
+        int statuscode = response.statusCode();
+
+        Assert.assertEquals(200, statuscode);
+       // response.prettyPrint();
+
+        String email = response.jsonPath().getString("responses[0].email");
+        Assert.assertFalse(email.isEmpty());
+
+        String email3 = response.jsonPath().getString("responses[2].email");
+        String email4 = response.jsonPath().getString("responses[3].email");
+
+
+}
+
+
+
+
+    @org.junit.Test
+    public void GetAllSellersLoop(){
+
+    String url = Config.getProperty("cashWiseApiUrl") + "/api/myaccount/sellers";
+    String token = CashWiseToken.GetToken();
+
+    Map<String, Object> lParams = new HashMap<>();
+    lParams.put("isArchived", false);
+    lParams.put("size", 10);
+    lParams.put("page", 1);
+
+
+    Response response = RestAssured.given().auth().oauth2(token).params(lParams).get(url);
+
+    int statuscode = response.getStatusCode();
+
+    Assert.assertEquals(200, statuscode);
+
+        List<String> ListofEmails = response.jsonPath().getList("responses.email");
+
+        for (String email: ListofEmails) {
+            Assert.assertFalse(email.isEmpty());
+        }
+
+
+
+
+
+
+}
+
+
+
+
+
+
 
 
 
--- src/test/java/entities/RequestBody.java
+++ src/test/java/entities/RequestBody.java
@@ -1,11 +1,20 @@
 package entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class RequestBody {
 
 
 
 private  String email;
 private String password;
+
+    @JsonProperty("CompanyName")
+    private String CompanyName;
+    private String SellerName;
+    private int PhoneNumber;
+    private String Address;
+
 
     public String getEmail() {
         return email;
@@ -22,4 +31,40 @@ private String password;
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getCompanyName() {
+        return CompanyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        CompanyName = companyName;
+    }
+
+    public String getSellerName() {
+        return SellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        SellerName = sellerName;
+    }
+
+    public int getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+
 }
+
+
